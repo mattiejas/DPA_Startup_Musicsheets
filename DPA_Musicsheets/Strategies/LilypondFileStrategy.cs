60a5feef02921eee1a75487024a67f51b2210a7d
Jeffrey <15036278@student.hhs.nl>
2018-09-25 09:39:44 +0200

    Implement basic Strategy Pattern

---
+++ DPA_Musicsheets/Strategies/LilypondFileStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+using DPA_Musicsheets.ViewModels;
+
+namespace DPA_Musicsheets.Strategies
+{
+    class LilypondFileStrategy : IFileStrategy
+    {
+        public string LilypondText { get; set; }
+        public LilypondViewModel LilypondViewModel { get; set; }
+
+        public void Handle(string filename)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var line in File.ReadAllLines(filename))
+            {
+                sb.AppendLine(line);
+            }
+
+            this.LilypondText = sb.ToString();
+            this.LilypondViewModel.LilypondTextLoaded(this.LilypondText);
+        }
+    }
+}
---
+++ DPA_Musicsheets/Strategies/MidiFileStrategy.cs
@@ -0,0 +1,36 @@
+﻿using Common.Interfaces;
+using DPA_Musicsheets.Managers;
+using Sanford.Multimedia.Midi;
+
+namespace DPA_Musicsheets.Strategies
+{
+    class MidiFileStrategy : IFileStrategy
+    {
+        private readonly IViewManagerPool _pool;
+        public Sequence MidiSequence { get; set; }
+
+        public MidiFileStrategy(IViewManagerPool pool)
+        {
+            _pool = pool;
+        }
+
+        public void Handle(string filename)
+        {
+            // TODO: Nadenken over de flow, want PSAMViewManager heeft een score nodig (dat wordt gegenereerd door een Sequence en MidiPlayer gebruikt juist de score om het om te zetten naar Midi (kortom twee keer hetzelfde gebeurd terwijl de sequence al hebben
+            MidiSequence = new Sequence();
+            MidiSequence.Load(filename);
+
+            // MidiPlayerViewModel.MidiSequence = MidiSequence;
+
+            // TODO: load lilypond text 
+            // this.LilypondText = LoadMidiIntoLilypond(MidiSequence);
+            // this.LilypondViewModel.LilypondTextLoaded(this.LilypondText);
+
+            var score = MidiManager.Load(MidiSequence);
+            foreach (var viewManager in _pool)
+            {
+                viewManager.Load(score);
+            }
+        }
+    }
+}
